7576c5eec99deeeb827f481cc57fce7e7843d807
vadimopanasenko <vadimopanasenka1@gmail.com>
2024-10-05 18:21:57 +0200

    hm4.3

---
+++ lesson.py
@@ -0,0 +1,22 @@
+import random
+
+num1 = []
+for i in range(random.randint(3, 10)):
+    num1.append(random.randint(1,9))
+num2 = [num1[0], num1[2], num1[-2]]
+print(num1, num2, sep="==")
+
+num1 = []
+for i in range(random.randint(3, 10)):
+    num1.append(random.randint(1,9))
+num2 = [num1[0], num1[2], num1[-2]]
+print(num1, num2, sep="==")
+
+num1 = []
+for i in range(random.randint(3, 10)):
+    num1.append(random.randint(1,9))
+num2 = [num1[0], num1[2], num1[-2]]
+print(num1, num2, sep="==")
+
+
+
